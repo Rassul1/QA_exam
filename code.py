e8f7c5b47a847619d7cbf67e1207b34201657a79
Rassul1 <72034424+Rassul1@users.noreply.github.com>
2022-05-13 22:49:54 +0500

    Add files via upload

---
+++ code.py
@@ -0,0 +1,43 @@
+class exam(object):
+    pass
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import unittest
+import pprint
+address = "http://the-internet.herokuapp.com/challenging_dom"
+driver_path = "D:\SE 2\PracticalPart\chromedriver.exe"
+
+
+class TestResults(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=driver_path)
+        self.wait = WebDriverWait(self.driver, 10)
+        self.address = address
+    
+    def test_header(self):
+        driver = self.driver
+        elem = driver.find_element(
+            By.XPATH, ('//div[@class="large-10 columns"]/table/thead/tr/th[1]').text == "Lorem" 
+        )
+
+    def test_open_page(self):
+        res = self.driver.get(address)
+        print("RESULT", self.driver.current_url, dir(self.driver))
+        self.assertIn(self.address, self.driver.current_url)
+        script = "return window.performance.getEntries();" 
+        perf = self.driver.execute_script(script)
+        print(perf)
+        pprint.pprint(perf)
+        with open(r"D:\SE 2\PracticalPart\result_perf.txt", "w") as fh:
+            for curr in perf:
+                fh.write(f"{curr['name']}, {curr['duration']}\n")
+        with open("result_perf.txt", "r") as fh:
+            print(fh.read())
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ exam.bat
@@ -0,0 +1,2 @@
+REM C:\Users\begen\first_env\Scripts\activate.bat
+C:\Users\begen\AppData\Local\Microsoft\WindowsApps\python.exe "D:\SE 2\PracticalPart\code.py"
